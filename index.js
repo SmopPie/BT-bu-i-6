0ad833de6adb2979a06bb335c35ee2623495f5d1
SmopPie <smopdoge000@gmail.com>
2022-10-01 12:55:35 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let giohang = {
+    apple:0,
+    melon:0,
+    snack:0,
+}
+function click_apple() {
+    giohang.apple ++
+    console.log(giohang)
+}
+function click_melon() {
+    giohang.melon = giohang.melon + 1
+    console.log(giohang)
+}
+function click_snack() {
+    giohang.melon +=1
+    console.log(giohang)
+}
